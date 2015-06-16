703b54dba75376e36daf05cee992083abdeb25eb
iyo <iyo@ToshimasaIto-no-MacBook-Air.local>
2015-06-17 08:20:13 +0900

    fix sync

--- sync.go
+++ sync.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"runtime"
 	"sync"
 	"time"
 )
@@ -20,7 +21,7 @@ type queue struct {
 	remain    int
 	rp        int
 	wp        int
-	mutex     *sync.Mutex
+	wg        *sync.WaitGroup
 	not_full  *sync.Cond
 	not_empty *sync.Cond
 }
@@ -31,11 +32,13 @@ type thread_arg struct {
 }
 
 func enqueue(q *queue, v int) {
-	q.mutex.Lock()
-	defer q.mutex.Unlock()
+	q.not_full.L.Lock()
+	defer q.not_full.L.Unlock()
 	for q.remain == MAX_QUEUE_NUM {
 		q.not_full.Wait()
 	}
+	fmt.Println(q.values)
+	fmt.Println(q.wp)
 	q.values[q.wp] = v
 	q.wp++
 	q.remain++
@@ -43,15 +46,18 @@ func enqueue(q *queue, v int) {
 		q.wp = 0
 	}
 	q.not_empty.Signal()
+	//fmt.Println(q.values)
 }
 
 func dequeue(q *queue, v *int) {
-	q.mutex.Lock()
-	defer q.mutex.Unlock()
+	q.not_empty.L.Lock()
+	defer q.not_empty.L.Unlock()
 	for q.remain == 0 {
 		q.not_empty.Wait()
 	}
+	fmt.Println(q.values)
 	*v = q.values[q.rp]
+	fmt.Printf("de : %v\n", *v)
 	q.rp++
 	q.remain--
 	if q.rp == MAX_QUEUE_NUM {
@@ -79,27 +85,31 @@ func consumer_func(args *thread_arg) {
 		if i == END_DATA {
 			break
 		}
-		fmt.Printf("[Consumer %d]     ==> %d \n", i)
+		fmt.Printf("con: %d\n", i)
+		fmt.Printf("[Consumer %d]     ==> %d \n", args.id, i)
 		time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
 	}
+	args.queue.wg.Done()
 	return
 }
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
 	var ptarg [THREAD_NUM]*thread_arg
 	var ctarg [THREAD_NUM]*thread_arg
 
-	var mutex = new(sync.Mutex)
 	var nf = new(sync.Mutex)
 	var not_full = sync.NewCond(nf)
 	var ne = new(sync.Mutex)
 	var not_empty = sync.NewCond(ne)
 	var q queue = queue{}
+	var wg = new(sync.WaitGroup)
 
 	q.rp = 0
 	q.wp = 0
 	q.remain = 0
-	q.mutex = mutex
+	q.wg = wg
 	q.not_full = not_full
 	q.not_empty = not_empty
 
@@ -110,11 +120,9 @@ func main() {
 
 	for i := 0; i < THREAD_NUM; i++ {
 		ctarg[i] = &thread_arg{i, &q}
+		wg.Add(1)
 		go consumer_func(ctarg[i])
 	}
-
-	for i := 0; i < THREAD_NUM; i++ {
-
-	}
-	time.Sleep(time.Second * 100)
+	wg.Wait()
+	//time.Sleep(time.Second * 100)
 }
