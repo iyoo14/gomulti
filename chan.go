073e6768b2d8102adbd2b7c733e1cff1b896f208
iyo <iyo@ToshimasaIto-no-MacBook-Air.local>
2015-06-26 07:39:45 +0900

    fix chan.go

--- chan.go
+++ chan.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/iyoo14/gomulti/entity"
 	"math/rand"
 	"runtime"
 	"sync"
@@ -16,56 +17,34 @@ const (
 	END_DATA        = -1
 )
 
-type queue struct {
-	values chan int
-	wg     *sync.WaitGroup
-	lock   *sync.Mutex
-}
-
 type thread_arg struct {
 	id    int
-	queue *queue
-}
-
-func enqueue(q *queue, v int, id int) {
-	q.values <- v
-}
-
-func dequeue(q *queue, id int) (v int) {
-	q.lock.Lock()
-	defer q.lock.Unlock()
-	for {
-		if len(q.values) != 0 {
-			break
-		}
-		time.Sleep(1 * time.Second)
-	}
-	return <-q.values
+	queue *entity.Queue
 }
 
 func producer_func(args *thread_arg) {
 	for i := 0; i < THREAD_DATA_NUM; i++ {
 		num := args.id*THREAD_DATA_NUM + i
-		enqueue(args.queue, num, args.id)
-		fmt.Printf("[Producer %d] ==> %d \n", args.id, num)
+		args.queue.Enqueue(num, args.id)
+		fmt.Printf("[Producer %d-%d] ==> %d \n", args.id, i, num)
 		time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
 	}
 
-	enqueue(args.queue, END_DATA, args.id)
+	args.queue.Enqueue(END_DATA, args.id)
 	return
 }
 
-func consumer_func(args *thread_arg) {
+func consumer_func(args *thread_arg, wg *sync.WaitGroup) {
 	var i int
 	for {
-		i = dequeue(args.queue, args.id)
+		i = args.queue.Dequeue(args.id)
 		if i == END_DATA {
 			break
 		}
 		fmt.Printf("[Consumer %d]     ==> %d \n", args.id, i)
 		time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
 	}
-	args.queue.wg.Done()
+	wg.Done()
 	return
 }
 
@@ -75,23 +54,18 @@ func main() {
 	var ptarg [THREAD_NUM]*thread_arg
 	var ctarg [THREAD_NUM]*thread_arg
 
-	var lock = new(sync.Mutex)
-	var q queue = queue{}
+	var q *entity.Queue = entity.NewQueue(MAX_QUEUE_NUM)
 	var wg = new(sync.WaitGroup)
 
-	q.lock = lock
-	q.values = make(chan int, MAX_QUEUE_NUM)
-	q.wg = wg
-
 	for i := 0; i < THREAD_NUM; i++ {
-		ptarg[i] = &thread_arg{i, &q}
+		ptarg[i] = &thread_arg{i, q}
 		go producer_func(ptarg[i])
 	}
 
 	for i := 0; i < THREAD_NUM; i++ {
-		ctarg[i] = &thread_arg{i, &q}
+		ctarg[i] = &thread_arg{i, q}
 		wg.Add(1)
-		go consumer_func(ctarg[i])
+		go consumer_func(ctarg[i], wg)
 	}
 	wg.Wait()
 	//time.Sleep(time.Second * 100)
---
+++ entity/queue.go
@@ -0,0 +1,34 @@
+package entity
+
+import (
+	"sync"
+	"time"
+)
+
+type Queue struct {
+	values chan int
+	lock   *sync.Mutex
+}
+
+func (q *Queue) Enqueue(v int, id int) {
+	q.values <- v
+}
+
+func (q *Queue) Dequeue(id int) (v int) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	for {
+		if len(q.values) != 0 {
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
+	return <-q.values
+}
+
+func NewQueue(num int) *Queue {
+	q := &Queue{}
+	q.values = make(chan int, num)
+	q.lock = new(sync.Mutex)
+	return q
+}
